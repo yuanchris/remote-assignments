51a9911b32871dcd79f13f42c3f6bcf75dd8bd11
Chris <56621625+yuanchris@users.noreply.github.com>
2020-04-27 12:47:25 +0800

    change max

--- Week-2/Assignment-1/Assignment-1.js
+++ Week-2/Assignment-1/Assignment-1.js
@@ -1,8 +1,6 @@
 function max(numbers) {
-    let maxNumber;
-    for (let i = 0; i < numbers.length; i++) {
-        if (i == 0)
-            maxNumber = numbers[i];
+    let maxNumber = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
         if (numbers[i] > maxNumber)
             maxNumber = numbers[i];
         
